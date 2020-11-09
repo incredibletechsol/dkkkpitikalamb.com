bbd2e81fbd4744f8d365818f1fac65a442d73859
Amit Bhalerao <incrallinonebackup@gmail.com>
2020-11-09 12:14:31 +0530

    Database properties updated

--- ControlPanel/conn.php
+++ ControlPanel/conn.php
@@ -1,9 +1,8 @@
 <?php
 $servername = "localhost";
-$username = "dkkkpmba_iti";
-$password = "Kasturi12#$";
-$dbName = "dkkkpmba_iti";
-
+$username = "kasturic_dkkkp";
+$password = "Kasturi254@#$";
+$dbName = "kasturic_dkkkpiti";
 
 // Create connection
 $conn =  mysqli_connect($servername, $username, $password, $dbName);
